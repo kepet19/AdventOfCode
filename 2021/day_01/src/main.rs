2c295598783760996187e76c2db0d37d65298222
kepet19 <kevinmeejach@gmail.com>
2021-12-02 17:02:02 +0100

    Fix day1 to be shorter

--- 2021/day_01/src/main.rs
+++ 2021/day_01/src/main.rs
@@ -6,71 +6,12 @@ fn main() {
 fn solution_2() -> usize {
     let numbers = read_numbers();
     let mut count = 0;
-    let mut previus_number = usize::MAX;
-    let mut sum_a = 0;
-    let mut sum_b = 0;
-    let mut sum_c = 0;
-    let mut sum_d = 0;
 
-    for (i, number) in numbers.iter().enumerate() {
-        let sum = match i % 4 {
-            0 => {
-                sum_a += number;
-
-                if i < 2 {
-                    continue;
-                }
-                sum_c += number;
-                sum_d += number;
-                let out_sum = sum_b;
-                sum_b = 0;
-                out_sum
+    for numbers in numbers.windows(4) {
+        if let [a, b, c, d] = numbers {
+            if a + b + c < b + c + d {
+                count += 1;
             }
-            1 => {
-                sum_a += number;
-                sum_b += number;
-
-                if i < 2 {
-                    continue;
-                }
-                sum_d += number;
-                let out_sum = sum_c;
-                sum_c = 0;
-                out_sum
-            }
-            2 => {
-                sum_a += number;
-                sum_b += number;
-                sum_c += number;
-
-                if i < 2 {
-                    continue;
-                }
-                let out_sum = sum_d;
-                sum_d = 0;
-                out_sum
-            }
-            3 => {
-                sum_b += number;
-                sum_c += number;
-                sum_d += number;
-
-                if i < 2 {
-                    continue;
-                }
-                let out_sum = sum_a;
-                sum_a = 0;
-                out_sum
-            }
-            _ => unreachable!(),
-        };
-
-        if sum > previus_number {
-            count += 1;
-            dbg!(&count, &sum);
-            previus_number = sum;
-        } else {
-            previus_number = sum;
         }
     }
     count
