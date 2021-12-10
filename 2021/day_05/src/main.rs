74426941726c72ff3953cf20c43c0a33f13fb6b8
kepet19 <kevinmeejach@gmail.com>
2021-12-10 11:34:33 +0100

    Fix day_05 part2

--- 2021/day_05/src/main.rs
+++ 2021/day_05/src/main.rs
@@ -2,25 +2,34 @@ use std::{collections::HashMap, convert, num::ParseIntError, str::FromStr};
 
 #[derive(Debug, PartialEq)]
 struct Line {
-    left: Point,
-    right: Point,
+    start: Point,
+    end: Point,
 }
 
 impl Line {
     fn iter(&self) -> LineIter {
         LineIter {
-            stop: self.right.clone(),
-            curr: self.left.clone(),
+            end: self.end.clone(),
+            curr: self.start.clone(),
+            stop_walk: false,
             walk_x: 0,
             walk_y: 0,
         }
     }
-}
 
+    fn is_straight(&self) -> bool {
+        self.start.x == self.end.x || self.start.y == self.end.y
+    }
+
+    fn is_45(&self) -> bool {
+        ((self.end.x - self.start.x) as i32).abs() == ((self.end.y - self.start.y) as i32).abs()
+    }
+}
 #[derive(Debug, PartialEq)]
 struct LineIter {
-    stop: Point,
+    end: Point,
     curr: Point,
+    stop_walk: bool,
     walk_x: isize,
     walk_y: isize,
 }
@@ -29,28 +38,39 @@ impl Iterator for LineIter {
     type Item = Point;
 
     fn next(&mut self) -> Option<Self::Item> {
+        if self.stop_walk {
+            return None;
+        }
         let curr = self.curr.clone();
 
         if self.walk_x == 0 && self.walk_y == 0 {
-            self.walk_x = if (self.stop.x - self.curr.x).is_negative() {
+            self.walk_x = if (self.end.x - self.curr.x).is_negative() {
                 -1
             } else {
-                1
+                if self.end.x - self.curr.x == 0 {
+                    0
+                } else {
+                    1
+                }
             };
-            self.walk_y = if (self.stop.y - self.curr.y).is_negative() {
+            self.walk_y = if (self.end.y - self.curr.y).is_negative() {
                 -1
             } else {
-                1
+                if self.end.y - self.curr.y == 0 {
+                    0
+                } else {
+                    1
+                }
             };
         }
 
-        if self.curr.y != self.stop.y && self.curr.x != self.stop.x {
-            self.curr.y += self.walk_y;
-            self.curr.x += self.walk_x;
-            Some(curr)
-        } else {
-            None
+        if self.curr.y == self.end.y && self.curr.x == self.end.x {
+            self.stop_walk = true;
         }
+        self.curr.y += self.walk_y;
+        self.curr.x += self.walk_x;
+
+        Some(curr)
     }
 }
 
@@ -58,38 +78,13 @@ impl FromStr for Line {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (left, right) = s
+        let (start, end) = s
             .split_once("->")
             .ok_or(Error::FailedToSplitStringFirst(s.to_string()))?;
-        let left: Point = left.parse()?;
-        let right: Point = right.parse()?;
+        let start: Point = start.parse()?;
+        let end: Point = end.parse()?;
 
-        // if left.x == right.x {
-        //     let (left, right) = if left.y > right.y {
-        //         (right, left)
-        //     } else {
-        //         (left, right)
-        //     };
-        //     return Ok(Self {
-        //         left,
-        //         right,
-        //         axis: crate::Axis::Y,
-        //     });
-        // }
-        // if left.y == right.y {
-        //     let (left, right) = if left.x > right.x {
-        //         (right, left)
-        //     } else {
-        //         (left, right)
-        //     };
-        //     return Ok(Self {
-        //         left,
-        //         right,
-        //         axis: crate::Axis::X,
-        //     });
-        // }
-
-        Ok(Self { left, right })
+        Ok(Self { start, end })
     }
 }
 
@@ -115,19 +110,41 @@ impl FromStr for Point {
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").expect("a valid file");
-    let lines: Vec<Line> = input.lines().flat_map(|v| v.parse()).collect();
+    dbg!(part1(&input));
+    dbg!(part2(&input));
+}
+
+fn part1(str: &str) -> usize {
+    let lines: Vec<Line> = str
+        .lines()
+        .flat_map(|v| v.parse::<Line>())
+        .filter(|v| v.is_straight())
+        .collect();
 
     let mut seen: HashMap<Point, usize> = HashMap::new();
     for line in lines.iter() {
         for point in line.iter() {
-            if seen.contains_key(&point) {
-                *seen.get_mut(&point).unwrap() += 1;
-            } else {
-                seen.insert(point, 1);
-            }
+            *seen.entry(point).or_insert(0) += 1;
         }
     }
-    dbg!(seen.iter().filter(|(_, v)| **v > 1).count());
+
+    seen.iter().filter(|(_, v)| **v > 1).count()
+}
+
+fn part2(str: &str) -> usize {
+    let lines: Vec<Line> = str
+        .lines()
+        .flat_map(|v| v.parse::<Line>())
+        .filter(|v| v.is_straight() || v.is_45())
+        .collect();
+
+    let mut seen: HashMap<Point, usize> = HashMap::new();
+    for line in lines.iter() {
+        for point in line.iter() {
+            *seen.entry(point).or_insert(0) += 1;
+        }
+    }
+    seen.iter().filter(|(_, v)| **v > 1).count()
 }
 
 #[derive(Debug)]
@@ -145,150 +162,50 @@ impl convert::From<ParseIntError> for Error {
 #[cfg(test)]
 mod test {
     use super::*;
-    // #[test]
-    // fn hit() {
-    //     assert_eq!(
-    //         Line {
-    //             left: Point { x: 0, y: 9 },
-    //             right: Point { x: 5, y: 9 },
-    //             axis: Axis::X,
-    //         }
-    //         .hit(&Line {
-    //             left: Point { x: 0, y: 9 },
-    //             right: Point { x: 5, y: 9 },
-    //             axis: Axis::X,
-    //         }),
-    //         0
-    //     );
-
-    //     assert_eq!(
-    //         Line {
-    //             left: Point { x: 1, y: 9 },
-    //             right: Point { x: 5, y: 9 },
-    //             axis: Axis::X,
-    //         }
-    //         .hit(&Line {
-    //             left: Point { x: 0, y: 9 },
-    //             right: Point { x: 5, y: 9 },
-    //             axis: Axis::X,
-    //         }),
-    //         5
-    //     );
-
-    //     assert_eq!(
-    //         Line {
-    //             left: Point { x: 5, y: 0 },
-    //             right: Point { x: 5, y: 4 },
-    //             axis: Axis::Y,
-    //         }
-    //         .hit(&Line {
-    //             left: Point { x: 5, y: 3 },
-    //             right: Point { x: 5, y: 9 },
-    //             axis: Axis::Y,
-    //         }),
-    //         2
-    //     );
-
-    //     // Lines cross -------
-    //     assert_eq!(
-    //         Line {
-    //             left: Point { x: 0, y: 4 },
-    //             right: Point { x: 5, y: 4 },
-    //             axis: Axis::X,
-    //         }
-    //         .hit(&Line {
-    //             left: Point { x: 5, y: 3 },
-    //             right: Point { x: 5, y: 9 },
-    //             axis: Axis::Y,
-    //         }),
-    //         1
-    //     );
-
-    //     assert_eq!(
-    //         Line {
-    //             left: Point { x: 0, y: 4 },
-    //             right: Point { x: 5, y: 4 },
-    //             axis: Axis::X,
-    //         }
-    //         .hit(&Line {
-    //             left: Point { x: 5, y: 0 },
-    //             right: Point { x: 5, y: 3 },
-    //             axis: Axis::Y,
-    //         }),
-    //         0
-    //     );
-    // }
-
-    // #[test]
-    // fn overlap_x() {
-    //     assert_eq!(
-    //         Line {
-    //             left: Point { x: 0, y: 9 },
-    //             right: Point { x: 5, y: 9 },
-    //             axis: Axis::X,
-    //         }
-    //         .overlap_x(&Line {
-    //             left: Point { x: 0, y: 9 },
-    //             right: Point { x: 2, y: 9 },
-    //             axis: Axis::X,
-    //         }),
-    //         3
-    //     );
-
-    //     assert_eq!(
-    //         Line {
-    //             left: Point { x: 0, y: 9 },
-    //             right: Point { x: 2, y: 9 },
-    //             axis: Axis::X,
-    //         }
-    //         .overlap_x(&Line {
-    //             left: Point { x: 4, y: 9 },
-    //             right: Point { x: 5, y: 9 },
-    //             axis: Axis::X,
-    //         }),
-    //         0
-    //     );
-    // }
 
     #[test]
-    fn overlap_y() {
-        //     assert_eq!(
-        //         Line {
-        //             left: Point { x: 5, y: 0 },
-        //             right: Point { x: 5, y: 3 },
-        //             axis: Axis::X,
-        //         }
-        //         .overlap_y(&Line {
-        //             left: Point { x: 5, y: 3 },
-        //             right: Point { x: 5, y: 4 },
-        //             axis: Axis::X,
-        //         }),
-        //         1
-        //     );
+    fn test_part1() {
+        assert_eq!(part1(INPUT), 5);
+    }
 
-        //     assert_eq!(
-        //         Line {
-        //             left: Point { x: 0, y: 3 },
-        //             right: Point { x: 0, y: 4 },
-        //             axis: Axis::X,
-        //         }
-        //         .overlap_y(&Line {
-        //             left: Point { x: 0, y: 2 },
-        //             right: Point { x: 0, y: 3 },
-        //             axis: Axis::X,
-        //         }),
-        //         1
-        //     );
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(INPUT), 12);
+    }
 
+    #[test]
+    fn test_iter() {
         assert_eq!(
             Line {
-                left: Point { x: 0, y: 3 },
-                right: Point { x: 0, y: 4 },
+                start: Point { x: 0, y: 3 },
+                end: Point { x: 0, y: 4 },
             }
             .iter()
+            .next()
+            .unwrap(),
+            Point { x: 0, y: 3 }
+        );
+        assert_eq!(
+            Line {
+                start: Point { x: 0, y: 3 },
+                end: Point { x: 0, y: 4 },
+            }
+            .iter()
+            .skip(1)
             .next()
             .unwrap(),
             Point { x: 0, y: 4 }
         );
     }
+
+    const INPUT: &str = "0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2";
 }
